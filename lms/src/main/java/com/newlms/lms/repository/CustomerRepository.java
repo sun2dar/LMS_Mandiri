f4919c518f5ac7c8c8c1a3d72f8d21f79ac43eed
BWS <bayuwidiasantoso@gmail.com>
2018-02-09 16:24:12 +0700

    delete all

--- lms/src/main/java/com/newlms/lms/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.newlms.lms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.newlms.lms.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
